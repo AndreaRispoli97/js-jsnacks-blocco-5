ec54584dcb904e530f29912f05f864261a19d5d5
AndreaRispoli97 <Andrea9715@outlook.it>
2025-03-12 18:17:21 +0100

    add: bonus snack 7

--- snack-7/script.js
+++ snack-7/script.js
@@ -21,8 +21,18 @@ const students = [
 
 // Recupera i dati dello studente con id 2
 
-const marioBanfi = students.find((student) => student.id === 2);
+// const marioBanfi = students.find((student) => student.id === 2);
 
-console.log(marioBanfi);
+// console.log(marioBanfi);
 
+//bonus
+let foundStudent = [];
+for (let i = 0; i < students.length; i++) {
+  if(students[i].id == 2){
+    // foundStudent.push(students[i]);
+    foundStudent = students[i];
+  }
+  
+}
+console.log(foundStudent);
 // Risultato: { id: 2, name: 'Mario Banfi', age: 34, class: '4A' }
